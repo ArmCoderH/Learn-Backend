4cd366034830476f1ac812d72cbe1a216d90ad2f
Arman Hingora <armanhingora555@gmail.com>
2025-01-29 20:30:16 +0530

    utils

--- arman-backend/src/index.js
+++ arman-backend/src/index.js
@@ -2,7 +2,7 @@ import dotenv  from 'dotenv'
 import mongoose from 'mongoose'
 import express from 'express'
 import {DB_NAME} from './constants.js'
-import connectDB from './db/index.js'
+import connectDB from './db/db.js'
 
 dotenv.config({
     path : './env'
